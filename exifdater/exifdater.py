9c31539f9ed1e837143785b4b63aa8f3aa49f0b3
Grzegorz Trochonowicz <trotylpl@gmail.com>
2025-05-17 11:45:48 +0100

    check library instalation

--- exifdater/exifdater.py
+++ exifdater/exifdater.py
@@ -3,9 +3,22 @@
 print("testExifDater 0.01")
 # plan
 # read parameters
+# readfile
 # read exif
 # extract date from EXIF
 # set date of file form EXIT
 # batch procesing
+# display information onscreen
+import importlib.util
+
+def is_library_installed(library_name):
+    return importlib.util.find_spec(library_name) is not None
+
+# Example usage
+library_name = 'numpy'
+if is_library_installed(library_name):
+   print(f"{library_name} is installed.")
+else:
+   print(f"{library_name} is not installed.")
 
 
